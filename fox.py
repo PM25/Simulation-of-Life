868d31c0f3b19e89e004f21b1df93af83fefdc8e
Tinypatrice <64115275+Tinypatrice@users.noreply.github.com>
2020-06-07 22:21:00 +0800

    Add files via upload

--- fox.py
+++ fox.py
@@ -16,30 +16,123 @@ walk_images = []  # 讀取動畫圖片
 for i in range(1, 10):
     fname = f"images/animation/FOX/fox{i}.png"
     walk_images.append(pg.transform.scale(pg.image.load(fname), (30, 30)))
+
+flip_walk_images = []  # 讀取動畫圖片
+for i in range(1, 10):
+    flip_walk_images.append(pg.transform.flip(walk_images[i - 1], True, False))
+
 eat_images = []  # 讀取動畫圖片
 for i in range(1, 6):
     fname = f"images/animation/FOX/foxeat{i}.png"
     eat_images.append(pg.transform.scale(pg.image.load(fname), (30, 30)))
 
+flip_eat_images = []
+for i in range(1, 6):
+    flip_eat_images.append(pg.transform.flip(eat_images[i - 1], True, False))
 
 # 狐狸
 class FoxSprite(pg.sprite.Sprite):
+    
     def __init__(self, x, y):
         super().__init__()
-        pass
+        self.index = 0
+        self.x = x
+        self.y = y
+        self.energy = 0
+        self.xStep = random.randint(-2, 2)
+        self.yStep = random.randint(-2, 2)
+        self.image = walk_images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.eating = False
+        self.eating_index = 0
+        group.add(self)
 
     def update(self):
-        pass
+        self.index += 1
+        if self.eating and self.t.alive():
+            self.eating_index += 1
+            if self.index >= len(eat_images):
+                self.index = 0
+            if self.xStep > 0:
+                self.image = eat_images[self.index]
+                self.xStep = 0.001
+            else:
+                self.image = flip_eat_images[self.index]
+                self.xStep = -0.001
+            self.yStep = 0
+
+            if self.eating_index > 70:
+                self.t.kill()
+                self.energy += 10
+                if self.energy > 100:
+                    self.birth(random.choice(["u", "d", "r", "l"]))
+                    self.energy = 0
+                self.eating = False
+                self.eating_index = 0
+                self.chase()
+                
+        else:
+            if self.index >= len(walk_images):
+                self.index = 0
+            if self.xStep > 0:
+                self.image = walk_images[self.index]
+            else:
+                self.image = flip_walk_images[self.index]
+
+            R = random.random()
+            if R < 0.01:
+                self.xStep = random.randint(-2, 2)
+                self.yStep = random.randint(-2, 2)
+                self.chase()
+
+            for t in pg.sprite.spritecollide(self, turtle.group, False):
+                self.t = t
+                self.eating = True
+                break
+            
+            
+        if pg.sprite.spritecollideany(self, block.horiz_walls):
+            self.yStep = -self.yStep
+            self.xStep = random.randint(-2, 2)
+        if pg.sprite.spritecollideany(self, block.vert_walls):
+            self.xStep = -self.xStep
+            self.yStep = random.randint(-2, 2)
+        self.x += self.xStep
+        self.y += self.yStep
+        
+        if self.x <= 30:
+            self.x = 30
+            self.xStep = random.randint(0, 1)
+        if self.y <= 30:
+            self.y = 30
+            self.yStep = random.randint(0, 1)
+        if self.x >= window_size[0] - 30:
+            self.x = window_size[0] - 30
+            self.xStep = random.randint(-1, 0)
+        if self.y >= window_size[1] - 30:
+            self.y = window_size[1] - 30
+            self.yStep = random.randint(-1, 0)
+        self.rect.center = [self.x, self.y]
+
+
 
     def birth(self, direction):
-        pass
+        if direction == "u":
+            fox = FoxSprite(self.x, self.y - 30)
+        if direction == "d":
+            fox = FoxSprite(self.x, self.y + 30)
+        if direction == "r":
+            fox = FoxSprite(self.x + 30, self.y)
+        if direction == "l":
+            fox = FoxSprite(self.x - 30, self.y)
 
     def chase(self):
         pos = pg.math.Vector2(self.x, self.y)
         if len(turtle.group) > 0:
             t = min(
                 [t for t in turtle.group],
-                key=lambda t: pos.distance_to(pg.math.Vector2(t.x, t.y)),
+                key=lambda t:pos.distance_to(pg.math.Vector2(t.x, t.y)),
             )
             distance = pos.distance_to(pg.math.Vector2(t.x, t.y))
             if distance < 80:
